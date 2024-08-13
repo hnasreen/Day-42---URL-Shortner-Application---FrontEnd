0d35b3448953eadfbc29a3fcce794a0b10d9a063
hnasreen <hnasreen1993@gmail.com>
2024-08-13 13:45:15 +0530

    changes done

--- src/components/UrlRedirects.js
+++ src/components/UrlRedirects.js
@@ -8,18 +8,25 @@ const UrlRedirects = () => {
   useEffect(() => {
     console.log("useEffect runs here")
     const fetchLongUrl = async () => {
-      const token = localStorage.getItem("token")
+      // const token = localStorage.getItem("token")
 
       try {
         const res = await axios.get(`http://localhost:5000/api/url/${shortUrl}`, {
           headers: {
-            'Authorization': `Bearer ${token}`,
+            // 'Authorization': `Bearer ${token}`,
             'Content-Type': 'application/json' // Add this if you're sending JSON data
           }
         });
-        console.log(res.data.longUrl, "longUrl")
-        console.log('shortUrl',shortUrl)
+        // console.log(res.data.longUrl, "longUrl")
+        // console.log('shortUrl',shortUrl)
         // window.location(res.data.longUrl);
+
+        if(res.data){
+          window.location.href=res.data
+        }
+        else{
+          throw new Error("Long URL not found")
+        }
 
       } catch (error) {
         console.error('Failed to fetch long URL:', error);
